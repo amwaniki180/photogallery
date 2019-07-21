9bcaaa103b055d132294a711882300091cf95179
Antony <amwaniki180@gmail.com>
2019-07-21 13:34:44 +0300

    created class category that includes delete and update function

--- photos/models.py
+++ photos/models.py
@@ -31,4 +31,30 @@ class Location(models.Model):
     def __str__(self):
         return self.photo_location
 
+class Category(models.Model):
+    """
+    This is the class where we will create categories
+    """
+    name = models.CharField(max_length = 30)
+    def save_category(self):
+        """
+        This is the function that we will use to save the instance of this class
+        """
+        self.save()
+
+    def delete(self):
+        """
+        This is the method to delete the instance
+        """
+        Category.objects.get(id = self.id).delete()
+
+    def update(self,field,val):
+        """
+        This is the method to update the instance
+        """
+        Category.objects.get(id = self.id).update(field = val)
+
+    def __str__(self):
+        return self.name
+
 
