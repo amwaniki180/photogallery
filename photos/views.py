985989861d5e91dca7623cf9ac18d9c0badbf35e
Antony <amwaniki180@gmail.com>
2019-07-21 17:59:38 +0300

    search

--- photos/views.py
+++ photos/views.py
@@ -28,3 +28,15 @@ def location_filter(request, location):
     title = f'{location} Photos'
     return render(request, 'location.html', {'title':title, 'images':images, 'locations':locations})
 
+def search(request):
+    locations = Location.objects.all()
+    if 'category' in request.GET and request.GET['category']:
+        search_term = request.GET.get('category')
+        images_found = Image.search_image(search_term)
+        message = f'{search_term}'
+
+        return render(request, 'search.html',{'message':message, 'images':images_found, 'locations':locations})
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{'message':message, 'locations':locations})
+
