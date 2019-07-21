b202a8dae6f72177d1955a798ca045260543c4d6
Antony <amwaniki180@gmail.com>
2019-07-21 14:18:32 +0300

    setup

--- photos/tests.py
+++ photos/tests.py
@@ -2,5 +2,30 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
+from .models import Image,Location,Category
+
 
 # Create your tests here.
+class ImageTestCases(TestCase):
+    """
+    This is the class we will use to test the images
+    """
+    
+    def setUp(self):
+        """
+        This will create a new image before each test case
+        """
+        funny = Category(name = "funny")
+        funny.save()
+        africa = Location(name = "Africa")
+        africa.save()
+        self.new_image = Image(name = "image",description = "h",location = africa,category = funny)
+    
+    def tearDown(self):
+        """
+        This will clear the db after each test
+        """
+        Image.objects.all().delete()
+        Category.objects.all().delete()
+        Location.objects.all().delete()
+
