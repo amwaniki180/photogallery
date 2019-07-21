f931c80f1c2b12c583f7b0df492f9557dcec1664
Antony <amwaniki180@gmail.com>
2019-07-21 17:00:48 +0300

    url pattern

--- photos/urls.py
+++ photos/urls.py
@@ -2,5 +2,11 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
-]
+    url('^$',views.index, name = 'home'),
+    url(r'^image/(?P<category_name>\w+)/(?P<image_id>\d+)',views.single_image, name='art'),
+    url(r'^location/(?P<location>\d+)', views.location_filter, name='location_filter'),
+    url(r'^search', views.search, name='search_images')
+    ]
+
+    if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
