17241320e3272e0d05852b84f0184dc1abd7ce35
Antony <amwaniki180@gmail.com>
2019-07-22 04:35:17 +0300

    base.html

--- photos/urls.py
+++ photos/urls.py
@@ -8,5 +8,5 @@ urlpatterns=[
     url(r'^search', views.search, name='search_images')
     ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
